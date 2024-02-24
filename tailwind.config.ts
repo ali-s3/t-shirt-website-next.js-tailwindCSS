1e8142d9e628c0512f8b896376c4c2a71a44dbed
ali-s3 <kashiknight99@gmail.com>
2024-02-25 00:41:24 +0500

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,25 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontSize: {
+        // default font size is 16px, we need to set it to 13px but a rem value
+        body: "0.8125rem", // 13px
+        h2: "1.25rem", // 20px
+        h1: "2.125rem", // 34px
+      },
+      colors: {
+        fuschia: {
+          60: "#FCDDEC",
+          80: "#F178B6",
+          100: "#EF5DA8",
+        },
+        iris: {
+          60: "#A5A6F6",
+          80: "#7879F1",
+          100: "#5D5FEF",
+        },
+        primary: "#2EBB77",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
